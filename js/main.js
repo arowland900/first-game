e7b02b1e37d8e0f4ecba14d72a94f0f203708b9a
Alexander Rowland <arowland900@gmail.com>
2018-05-23 10:38:23 -0700

    both players move corretly

--- js/main.js
+++ js/main.js
@@ -7,63 +7,92 @@ var $gameboard = $('#gameboard')
 var $startButton = $('#start-button')
 var $timer = $('#timer')
 var $score = $('#score')
+var $water = $('.water')
 
 var timeLeft = 60
 var scoreNum = 0
+var movementInterval
+var p1left = false
+var p1right = false
+var p2left = false
+var p2right = false
 
-$(window).on('keydown', function(event) {
+$('body').on('keydown', function(event) {
     const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-    if (930 > Number($playerOne.css("left").slice(0,-2)) && Number($playerOne.css("left").slice(0,-2)) > 10){
+    if (930 > Number($playerOne.css("left").slice(0,-2))){
+         if (key === "ArrowRight") {
+            // $playerOne.css("left", "+=20")
+            p1right = true
+        }
+    }
+    // if (930 < Number($playerOne.css("left").slice(0,-2))){
+    //     if (key === "ArrowRight") {
+    //        // $playerOne.css("left", "+=20")
+    //        p1right = false
+    //    }
+    // }
+    if(Number($playerOne.css("left").slice(0,-2)) > 20) {
         if (key === "ArrowLeft") {
-            $playerOne.css("left", "-=20")
-        } 
-        else if (key === "ArrowRight") {
-            $playerOne.css("left", "+=20")
-        }
-    }
-    else if (Number($playerOne.css("left").slice(0,-2)) === 930) {
-        if (key === "ArrowLeft") {
-            $playerOne.css("left", "-=20")
+            // $playerOne.css("left", "-=20")
+            p1left = true
         } 
     }
-    else if (Number($playerOne.css("left").slice(0,-2)) === 10) {
-        if (key === "ArrowRight") {
-            $playerOne.css("left", "+=20")
-        }
-    }
-});
-
-
-$(window).on('keydown', function(event) {
-    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-    if (940 > Number($playerTwo.css("left").slice(0,-2)) && Number($playerTwo.css("left").slice(0,-2)) > 10){
-        if (key === "z") {
-            $playerTwo.css("left", "-=30")
-        } 
-        else if (key === "c") {
-            $playerTwo.css("left", "+=30")
-        }
-    }
-    else if (Number($playerTwo.css("left").slice(0,-2)) === 940) {
-        if (key === "z") {
-            $playerTwo.css("left", "-=30")
-        } 
-    }
-    else if (Number($playerTwo.css("left").slice(0,-2)) === 10) {
+    // if(Number($playerOne.css("left").slice(0,-2)) < 20) {
+    //     if (key === "ArrowLeft") {
+    //         // $playerOne.css("left", "-=20")
+    //         p1left = false
+    //     } 
+    // }
+    if (930 > Number($playerTwo.css("left").slice(0,-2))){
         if (key === "c") {
-            $playerTwo.css("left", "+=30")
+            // $playerTwo.css("left", "+=30")
+            p2right = true
         }
     }
+    // if (930 < Number($playerTwo.css("left").slice(0,-2))){
+    //     if (key === "c") {
+    //         // $playerTwo.css("left", "+=30")
+    //         p2right = false
+    //     }
+    // }
+    if (Number($playerTwo.css("left").slice(0,-2)) > 20){
+        if (key === "z") {
+            // $playerTwo.css("left", "-=30")
+            p2left = true
+        } 
+    }
+    // if (Number($playerTwo.css("left").slice(0,-2)) < 20){
+    //     if (key === "z") {
+    //         // $playerTwo.css("left", "-=30")
+    //         p2left = false
+    //     } 
+    // }
 });
+
+$('body').on('keyup', function(event) {
+    const key = event.key;
+    if (key === "ArrowRight") {
+        p1right = false
+    }
+    if (key === "ArrowLeft") {
+        p1left = false
+    } 
+    if (key === "c") {
+        p2right = false
+    }
+    if (key === "z") {
+        p2left = false
+    } 
+})
 
 function RainSimulator() {
     var $newRain = $('<div>').addClass('blue-box')
     $newRain.css({
-        left: Math.random() * 930
+        left: Math.random() * 930 + 10
     })
     $gameboard.append($newRain);
     var rainMovement = setInterval(function() {
-        $newRain.css({top: '+=1px'});
+        $newRain.css({top: '+=1.2px'});
         if(($newRain.offset().top + $newRain.height()) > $gameboard.offset().top + $gameboard.height()) {
             $newRain.remove()
             clearInterval(rainMovement)
@@ -89,13 +118,15 @@ function RainSimulator() {
 function DirtSimulator() {
     var $newDirt = $('<div>').addClass('yellow-box')
     $newDirt.css({
-        left: Math.random() * 930
+        left: Math.random() * 930 + 10
     })
     $gameboard.append($newDirt);
     var dirtMovement = setInterval(function() {
         $newDirt.css({top: '+=2px'});
         if(($newDirt.offset().top + $newDirt.height()) > $gameboard.offset().top + $gameboard.height()) {
             $newDirt.remove()
+            decrementer()
+            decrementer()
             clearInterval(dirtMovement)
         } 
         else if ($newDirt.offset().left < ($playerOne.offset().left + $playerOne.width()) &&
@@ -118,24 +149,25 @@ function DirtSimulator() {
     }, 10);
 }
 
-function AddWater() {
-    var $newWater = $('<div>').addClass('water')
-    // $newDirt.css({
-    //     bottom: Math.random() * 930
-    // })
-    $bucket.append($newDirt);
-    
-}
+// function AddWater() {
+//     var $newWater = $('<div>').addClass('water')
+//     if (incrementer){ 
+//         $bucket.append($newWater);
+//     }
+//     else if (decrementer){
+//         $bucket.remove($newWater);
+//     }
+// }
   
 function startGame() {
     $startButton.off("click", startGame)
-    var moreDirt = setInterval(DirtSimulator, 3300)
+    var moreDirt = setInterval(DirtSimulator, 2700)
     var moreRain = setInterval(RainSimulator, 1500)
     var myTimer = setInterval(function(){
         timeLeft -= 1
         $timer.html('Time Left: '+ timeLeft)
         if (timeLeft === 0){
-            if (scoreNum >= 40){
+            if (scoreNum >= 50){
                 winAlert()
             }
             else {
@@ -144,6 +176,9 @@ function startGame() {
             clearInterval(myTimer)
             clearInterval(moreDirt)
             clearInterval(moreRain)
+            clearInterval(movementInterval)
+            $('.blue-box').remove()
+            $('.yellow-box').remove()
             timeLeft = 60
             $timer.html('Time Left: '+ timeLeft)
             scoreNum = 0
@@ -151,16 +186,28 @@ function startGame() {
             $startButton.on("click", startGame)
         }
     },1000);
+    movementInterval = setInterval(function(){
+        if(p1right) $playerOne.css("left", "+=1.5")
+        if($playerOne.css("left") == "940px") p1right = false
+        if(p1left) $playerOne.css("left", "-=1.5")
+        if($playerOne.css("left") == "10px") p1left = false
+        if(p2right) $playerTwo.css("left", "+=1.5")
+        if($playerTwo.css("left") == "940px") p2right = false
+        if(p2left) $playerTwo.css("left", "-=1.5")
+        if($playerTwo.css("left") == "10px") p2left = false
+    }, 1)
 }
 $startButton.on("click", startGame)
 
 function incrementer (){
     scoreNum += 1 
     $score.html('Score: ' + scoreNum)
+    $water.animate({height: scoreNum*2 + '%'})
 }
 function decrementer (){
     scoreNum -= 1 
     $score.html('Score: ' + scoreNum)
+    $water.animate({height: scoreNum*2+ '%'})
 }
 function reset () {
     scoreNum = 0
@@ -170,7 +217,11 @@ function winAlert () {
     alert(`You collected ${scoreNum} clean water units! You saved the day!`)
 } 
 function loseAlert () {
-    alert(`You collected ${scoreNum} clean water units.  Not enough.  MOOGLEBOOK WINS.`)
+    alert(`Almost! You collected ${scoreNum} clean water units. Nice try.`)
 }
 
 
+
+
+// figure out how to make raindrops look more like raindrops with border radius
+// figure out how to use both players simultaneously
