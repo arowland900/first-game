3b333e40f69b39331a2ead459170936b01bfeb90
Alexander Rowland <arowland900@gmail.com>
2018-05-23 16:58:17 -0700

    gh pages commit

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var $timer = $('#timer')
 var $score = $('#score')
 var $water = $('.water')
 
-var timeLeft = 60
+var timeLeft = 99
 var scoreNum = 0
 var movementInterval
 var p1left = false
@@ -18,28 +18,24 @@ var p2left = false
 var p2right = false
 
 $('body').on('keydown', function(event) {
-    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+    const key = event.key; // "ArrowRight", "ArrowLeft"
     if (940 > Number($playerOne.css("left").slice(0,-2))){
          if (key === "ArrowRight") {
-            // $playerOne.css("left", "+=20")
             p1right = true
         }
     }
     if(Number($playerOne.css("left").slice(0,-2)) > 20) {
         if (key === "ArrowLeft") {
-            // $playerOne.css("left", "-=20")
             p1left = true
         } 
     }
     if (940 > Number($playerTwo.css("left").slice(0,-2))){
         if (key === "c") {
-            // $playerTwo.css("left", "+=30")
             p2right = true
         }
     }
     if (Number($playerTwo.css("left").slice(0,-2)) > 20){
         if (key === "z") {
-            // $playerTwo.css("left", "-=30")
             p2left = true
         } 
     }
@@ -68,9 +64,10 @@ function RainSimulator() {
     })
     $gameboard.append($newRain);
     var rainMovement = setInterval(function() {
-        $newRain.css({top: '+=1.2px' });
+        $newRain.css({top: '+=2px' });
         if(($newRain.offset().top + $newRain.height()) > $gameboard.offset().top + $gameboard.height()) {
             $newRain.remove()
+            decrementer()
             clearInterval(rainMovement)
         } 
         else if ($newRain.offset().left < ($playerOne.offset().left + $playerOne.width()) &&
@@ -79,15 +76,16 @@ function RainSimulator() {
         $newRain.offset().top + $newRain.height() > $playerOne.offset().top){
             $newRain.remove()
             incrementer()
+            incrementer()
             clearInterval(rainMovement)
         }
-        else if ($newRain.offset().left < ($playerTwo.offset().left + $playerTwo.width()) &&
-        $newRain.offset().left + $newRain.width() > $playerTwo.offset().left &&
-        $newRain.offset().top < ($playerTwo.offset().top + $playerTwo.height()) &&
-        $newRain.offset().top + $newRain.height() > $playerTwo.offset().top){
-            $newRain.remove()
-            clearInterval(rainMovement)
-        }
+        // else if ($newRain.offset().left < ($playerTwo.offset().left + $playerTwo.width()) &&
+        // $newRain.offset().left + $newRain.width() > $playerTwo.offset().left &&
+        // $newRain.offset().top < ($playerTwo.offset().top + $playerTwo.height()) &&
+        // $newRain.offset().top + $newRain.height() > $playerTwo.offset().top){
+        //     $newRain.remove()
+        //     clearInterval(rainMovement)
+        // }
     }, 10);
 }
 
@@ -98,7 +96,7 @@ function DirtSimulator() {
     })
     $gameboard.append($newDirt);
     var dirtMovement = setInterval(function() {
-        $newDirt.css({top: '+=2px'});
+        $newDirt.css({top: '+=3px'});
         if(($newDirt.offset().top + $newDirt.height()) > $gameboard.offset().top + $gameboard.height()) {
             $newDirt.remove()
             decrementer()
@@ -125,13 +123,13 @@ function DirtSimulator() {
 
 function startGame() {
     $startButton.off("click", startGame)
-    var moreDirt = setInterval(DirtSimulator, 2700)
-    var moreRain = setInterval(RainSimulator, 1300)
+    var moreDirt = setInterval(DirtSimulator, 2500)
+    var moreRain = setInterval(RainSimulator, 700)
     var myTimer = setInterval(function(){
         timeLeft -= 1
-        $timer.html('Time Left: '+ timeLeft)
+        $timer.html('Time: '+ timeLeft)
         if (timeLeft === 0){
-            if (scoreNum >= 25){
+            if (scoreNum >= 100){
                 winAlert()
             }
             else {
@@ -144,24 +142,24 @@ function startGame() {
             $('.blue-box').remove()
             $('.yellow-box').remove()
             $water.animate({height: 0})
-            timeLeft = 60
-            $timer.html('Time Left: '+ timeLeft)
+            timeLeft = 99
+            $timer.html('Time: '+ timeLeft)
             scoreNum = 0
             $score.html('Score: ' + scoreNum)
             $startButton.on("click", startGame)
         }
     },1000);
     movementInterval = setInterval(function(){
-        if(p1right) $playerOne.css("left", "+=2")
+        if(p1right) $playerOne.css("left", "+=3")
         if($playerOne.css("left") == "940px") p1right = false
 
-        if(p1left) $playerOne.css("left", "-=2")
+        if(p1left) $playerOne.css("left", "-=3")
         if($playerOne.css("left") == "10px") p1left = false
 
-        if(p2right) $playerTwo.css("left", "+=2")
+        if(p2right) $playerTwo.css("left", "+=2.5")
         if($playerTwo.css("left") == "940px") p2right = false
 
-        if(p2left) $playerTwo.css("left", "-=2")
+        if(p2left) $playerTwo.css("left", "-=2.5")
         if($playerTwo.css("left") == "10px") p2left = false
     }, 1)
 }
@@ -170,26 +168,22 @@ $startButton.on("click", startGame)
 function incrementer (){
     scoreNum += 1 
     $score.html('Score: ' + scoreNum)
-    $water.animate({height: scoreNum*4 + '%'})
+    $water.animate({height: scoreNum + '%'})
 }
 function decrementer (){
     scoreNum -= 1 
+    if (scoreNum <= 0) scoreNum = 0
     $score.html('Score: ' + scoreNum)
-    $water.animate({height: scoreNum*4 + '%'})
+    $water.animate({height: scoreNum + '%'})
 }
 function reset () {
-    scoreNum = 0
+    scoreNum -= 10
+    if (scoreNum <= 0) scoreNum = 0
     $score.html('Score: ' + scoreNum)
 }
 function winAlert () {
-    alert(`You collected ${scoreNum} clean water units! You saved Earth!`)
+    alert(`Victory! Score: ${scoreNum}`)
 } 
 function loseAlert () {
-    alert(`You collected ${scoreNum} clean water units. You did't save earth :(.`)
+    alert(`Not enough water collected :( Score: ${scoreNum}`)
 }
-
-
-
-
-// figure out how to make raindrops look more like raindrops with border radius
-// figure out how to use both players simultaneously
